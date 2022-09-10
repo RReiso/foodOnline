29296c34673f76b30a50f490da1c3f922af30e4c
Ruta Reisoglu <rutareiso@protonmail.com>
2022-09-09 23:29:42 -0400

    Configure MEDIA files

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -137,6 +137,10 @@ STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
