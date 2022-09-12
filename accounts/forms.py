f1af6f81d2c9a1ab596afe07df2a99161664682a
Ruta Reisoglu <rutareiso@protonmail.com>
2022-09-12 14:10:26 -0400

    Display form errors

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,3 +10,12 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'username',
                   'email', 'password']
+
+    # overriding the built in method:
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password and confirmation don't match!")  # this will appear in html as form.non_field_errors
--- accounts/views.py
+++ accounts/views.py
@@ -10,14 +10,30 @@ def registerUser(request):
     if request.method == 'POST':
         print(request.POST)
         form = UserForm(request.POST)
-        # if form is valid sav in db
+        # if form is valid sav in db:
         if form.is_valid():
-            # form is ready to be saved and data is assigned to user var
+            # comes from clean method in UserForm (built in method)
+            password = form.cleaned_data['password']
+            # form is ready to be saved and data is assigned to user var:
             user = form.save(commit=False)
+            user.set_password(password)  # hash the password
             user.role = User.CUSTOMER
             user.save()
-            print('USSSER', user)
+
+            # alternative way to create a user - using create_user method:
+            # first_name = form.cleaned_data['first_name']
+            # last_name = form.cleaned_data['last_name']
+            # username = form.cleaned_data['username']
+            # email = form.cleaned_data['email']
+            # password = form.cleaned_data['password']
+            # user = User.objects.create_user(
+            #     first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            # user.role = User.CUSTOMER
+            # user.save()
             return redirect('registerUser')
+        else:
+            print('Invalid form')
+            print(form.errors)
     else:
         form = UserForm()
 
